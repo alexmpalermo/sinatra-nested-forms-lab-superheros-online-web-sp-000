b75d21fbfc5bcb89877a26af485ce09b85e4dbba
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-28 01:31:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,13 @@ class App < Sinatra::Base
   end 
   
   post '/teams' do
-    @team = Team.new(params[:name])
+    @team = Team.new(params[:team])
     
-    
+     params[:team][:members].each do |details|
+        Hero.new(details)
+      end
+
+       @heroes = Hero.all
     
     erb :team
   end
