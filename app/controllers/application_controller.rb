9d2f2c6c3c3b9e42d9b8abcc6667b59ee3313264
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-28 01:25:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do
+    erb :super_hero
+  end
+  
+  get '/super_hero' do
+    
+  end 
+  
+  post '/teams' do
+    
+    erb :team
+  end
 
 end
